327f519df784bdf93343889d5090ce7a84004ca7
Robin Betz <robin@robinbetz.com>
2015-10-21 15:18:07 -0700

    add stl loader

--- app/5ht2b/display_protein.js
+++ app/5ht2b/display_protein.js
@@ -21,12 +21,18 @@ controls.addEventListener( 'change', render );
 var parent = new THREE.Object3D();
 
 // load ascii model
-var jsonLoader = new THREE.JSONLoader();
-jsonLoader.load( "5ht2b/5HT2B.json", createScene );
-function createScene( geometry, materials ) {
-    var mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-    parent.add(mesh);
-}
+
+var loader = new THREE.STLLoader();
+loader.load( '5ht2b/5ht2b.stl', function ( geometry ) {
+    var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set( 0, - 0.25, 0.6 );
+    mesh.rotation.set( 0, - Math.PI / 2, 0 );
+    mesh.scale.set( 0.5, 0.5, 0.5 );
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    parent.add( mesh );
+} );
 
 scene.add(parent);
 
